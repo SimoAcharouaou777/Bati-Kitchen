48371d393068a2aeb2f67e13009d7a3d1b37efb3
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-09-20 12:24:33 +0200

    save quote function

--- src/Repository/QuoteRepository.java
+++ src/Repository/QuoteRepository.java
@@ -1,4 +1,26 @@
 package Repository;
 
+import Model.Quote;
+import Utils.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class QuoteRepository {
+    private Connection connection = DatabaseConnection.getInstance().getConnection();
+
+    public void addQuote(Quote quote){
+        String sql = "INSERT INTO quotes (project_id, estimated_amount,issue_date,validity_date,is_accepted) VALUES(?,?,?,?,?)";
+        try(PreparedStatement stmt = connection.prepareStatement(sql)){
+            stmt.setInt(1,quote.getProjectId());
+            stmt.setDouble(2,quote.getEstimatedAmount());
+            stmt.setDate(3,java.sql.Date.valueOf(quote.getIssueDate()));
+            stmt.setDate(4,java.sql.Date.valueOf(quote.getValidityDate()));
+            stmt.setBoolean(5,quote.isAccepted());
+            stmt.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
